f8116d6066f17861e7a239c623cfbc9dc9cd45b3
3devweb <email@3dev.com.br>
2020-01-10 15:44:51 -0300

    alterações no layout

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -3,16 +3,27 @@
         <meta charset="UTF-8" />
         <title>Inforce - Software, Site Personalizado e Marketing Digital Avançado
         </title>
-<meta name="description" content="Consultoria e Desenvolvimento Web especializada em Portais, Sites Personalizados, Softwares, Apps e Estratégias de Marketing Digital." />
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Consultoria e Desenvolvimento Web especializada em Portais, Sites Personalizados, Softwares, Apps e Estratégias de Marketing Digital." />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo asset('assets/css/style.css')?>" type="text/css">
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="<?php echo asset('assets/js/script.js')?>"></script>
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script>
+        $('#form-conversor').on('submit', function('event'){
+            event.preventDefault()
+        })
+        </script>
     </head>
     <body>
+        <div class="Container All">
+            <div class="col-lg-12 col-md-10 col-sm-12">
         <div class="Container Banner ">
             <div class="Content Homme">
                 <div class="col-lg-12 col-md-10">
@@ -25,23 +36,25 @@
                     </h1>
                 </div>
                 <div class="row">
-                    <button class="Button CaluladoraHome"> Calcule já </button>
+                   <a href="#calculadora"> <button class="btn-converter"> Calcule já </button></a>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
         <div class="Container Exchange">
             <div class="Content Exchange">
                 <div class="col-lg-12 col-md-10">
                 <div class="row">
-                    <h1></span>Exchange</span> Calculadora</h1>
+                    <h1></span class="ExchangeTitle">Exchange</span> Calculadora</h1>
                 </div>
                 <div class="row">
                     <p>Mais uma ferramenta disponivel no Portal Exchange Inforce</p>
                 </div>
-                <div class="Calculadora">
+                <div class="Calculadora" id="calculadora">
                 <div class="row">
-                    <form class="form-horizontal" action=>
+                    <form class="form-horizontal" id="form-conversor">
                     <input placeholder="quantidade" id="value" name="value" required="true">
                         <select name="base" required="true" id="base" class="FormataCampo">
                             <option  value="" disabled selected> escolha sua moeda</option>
@@ -62,7 +75,7 @@
                             <option value="BRL" id="BRL"> BRL </option>
                             <option value="EUR" id="EUR"> EUR </option>
                         </select>
-                        <button type="submit" id="calc" name="calc">Converter</button>
+                        <button class="btn-converter" id="calc">Converter</button>
                     </form>
                 </div>
                 <div class="row">
@@ -70,7 +83,7 @@
                 </div>
             </div>
             <div class="row">
-                <h1></span>Exchange</span> RealTime</h1>
+                <h1></span class="ExchangeTitle">Exchange</span> RealTime</h1>
             </div>
             <div class="RealTime">
                 <div class="row">
@@ -81,6 +94,17 @@
         </div>
     </div>
     </div>
+
+    <script>
+        $.ajax({
+   url:"/api/converter",
+   data: $("#form-conversor").serialize()
+}).then(function (data) {
+    var valor = JSON.parse(data)
+    $('#resultado').val(valor.value)
+})
+</script>
+
     </body>
     </html>
     
